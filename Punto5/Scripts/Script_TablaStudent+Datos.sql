8ba55252907de420aabe2145bce0b862aefe90cc
nicocespedesv <ncespedesvar@gmail.com>
2022-10-09 19:11:36 -0500

    Scripts punto 5

---
+++ Punto5/Scripts/Script_TablaStudent+Datos.sql
@@ -0,0 +1,24 @@
+CREATE TABLE Student(
+ID INT IDENTITY PRIMARY KEY,
+NOMBRE VARCHAR(50),
+APELLIDO VARCHAR(50),
+EMAIL VARCHAR(50),
+EDAD VARCHAR(2),
+)
+
+INSERT INTO Student VALUES('Andrés', 'Cediel', 'ac@gmail.com', '20')
+INSERT INTO Student VALUES('Andrés', 'Cediel', 'ac@gmail.com', '21')
+INSERT INTO Student VALUES('Andrés', 'Cediel', 'ac@gmail.com', '22')
+INSERT INTO Student VALUES('Andrés', 'Cediel', 'ac@gmail.com', '23')
+INSERT INTO Student VALUES('Andrés', 'Cediel', 'ac@gmail.com', '24')
+INSERT INTO Student VALUES('Andrés', 'Cediel', 'ac@gmail.com', '25')
+INSERT INTO Student VALUES('Andrés', 'Cediel', 'ac@gmail.com', '26')
+INSERT INTO Student VALUES('Andrés', 'Cediel', 'ac@gmail.com', '27')
+INSERT INTO Student VALUES('Andrés', 'Cediel', 'ac@gmail.com', '28')
+INSERT INTO Student VALUES('Andrés', 'Cediel', 'ac@gmail.com', '29')
+INSERT INTO Student VALUES('Andrés', 'Cediel', 'ac@gmail.com', '30')
+INSERT INTO Student VALUES('Andrés', 'Cediel', 'ac@gmail.com', '31')
+INSERT INTO Student VALUES('Andrés', 'Cediel', 'ac@gmail.com', '32')
+INSERT INTO Student VALUES('Andrés', 'Cediel', 'ac@gmail.com', '33')
+INSERT INTO Student VALUES('Andrés', 'Cediel', 'ac@gmail.com', '34')
+INSERT INTO Student VALUES('Andrés', 'Cediel', 'ac@gmail.com', '35')

b6dcd1167799cb9155b4366670f9af236e309723
nicocespedesv <ncespedesvar@gmail.com>
2022-10-10 09:56:59 -0500

    Factorial

---
+++ Factorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Factorial
+{
+    internal class Program
+    {
+        public static int Factorial(int number)
+        {
+            int fact = 0;
+            if (number == 0 || number == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                fact = number * Factorial(number - 1);
+            }
+            return fact;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Factorial(10).ToString());
+        }
+    }
+}
